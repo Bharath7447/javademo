5faff33add558af1ee36c5679a6ef82cdda57536
Bharath <96695660+Bharath7447@users.noreply.github.com>
2024-08-01 12:34:33 +0530

    Update FirstAPI.java

--- src/main/java/com/atpl/demo/FirstAPI.java
+++ src/main/java/com/atpl/demo/FirstAPI.java
@@ -10,7 +10,7 @@ public class FirstAPI {
 	public String ping()   
 	{  
 	 
-	return "OK";  
+	return "OK Fine";  
 	}  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
